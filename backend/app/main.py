7acc3c4a205605bd19ecc8f1516e85899c115be9
bala-codez <mohan144004@gmail.com>
2026-03-11 19:27:23 +0530

    feat(features/auth) : import auth , oauth router in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,18 @@
 from sys import api_version
 
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
+
 from app.core.config import settings
 from fastapi.middleware.cors import CORSMiddleware
 from app.controllers.auth_controller import router as auth_router
 from app.controllers.oauth_controller import router as oauth_router
-app = FastAPI(title=settings.APP_NAME)
+app = FastAPI(
+    title=settings.APP_NAME,
+    version="1.0.0",
+    docs_url="/docs" if settings.DEBUG else None,
+    redoc_url="/redoc" if settings.DEBUG else None
+)
 
 # allow frontend origin
 app.add_middleware(
@@ -16,5 +23,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# oauth session
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=settings.JWT_SECRET_KEY
+)
+
+# routers
 app.include_router(auth_router , prefix=settings.API_VERSION)
 app.include_router(oauth_router , prefix=settings.API_VERSION)
+
+# simple api testing
+@app.get(settings.API_VERSION)
+def test():
+    return {"message": "Hello World"}
