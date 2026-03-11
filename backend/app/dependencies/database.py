dd092a1ffefb1fc9c7576fdfed62f3ad5036e48b
bala-codez <mohan144004@gmail.com>
2026-03-11 19:14:10 +0530

    feat(features/auth) : implement dependencies injection in backend

---
+++ backend/app/dependencies/database.py
@@ -0,0 +1,8 @@
+from app.core.database import sessionLocal
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ backend/app/dependencies/services.py
@@ -0,0 +1,31 @@
+from fastapi import Depends
+
+from app.dependencies.repositories import get_user_repository
+from app.repositories.user_repository import UserRepository
+from app.services.auth_service import AuthService
+from app.services.email_service import EmailService
+from app.services.jwt_service import JWTService
+from app.services.oauth_service import OAuthService
+
+
+def get_email_service() -> EmailService:
+    return EmailService()
+
+
+def get_jwt_service() -> JWTService:
+    return JWTService()
+
+
+def get_auth_service(
+    user_repo: UserRepository = Depends(get_user_repository),
+    email_service: EmailService = Depends(get_email_service),
+    jwt_service: JWTService = Depends(get_jwt_service),
+) -> AuthService:
+    return AuthService(user_repo, email_service, jwt_service)
+
+
+def get_oauth_service(
+    user_repo: UserRepository = Depends(get_user_repository),
+    jwt_service: JWTService = Depends(get_jwt_service),
+) -> OAuthService:
+    return OAuthService(user_repo, jwt_service)
