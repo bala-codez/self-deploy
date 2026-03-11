600eb39738ae57e6d84b57741bc6092d8d05a2a6
bala-codez <mohan144004@gmail.com>
2026-03-11 19:19:34 +0530

    feat(features/auth) : adding more settings on backend

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,20 +4,30 @@ class Settings(BaseSettings):
 
     APP_NAME: str
     API_VERSION : str
+
     DATABASE_HOST :str
     DATABASE_PORT :int
     DATABASE_USER :str
     DATABASE_PASSWORD :str
     DATABASE_NAME : str
+
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str
     ACCESS_TOKEN_EXPIRE: int
     REFRESH_TOKEN_EXPIRE: int
-    SMTP_USER: str
-    SMTP_PASSWORD: str
+
+    RESEND_API_KEY : str
+    EMAIL_FROM : str
+
+    OTP_EXPIRE_MINUTES: int
+
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
+    GOOGLE_REDIRECT_URI: str
+
     FRONTEND_ORIGINS: str
+    FRONTEND_URL: str
+
     DEBUG: bool = False
 
     model_config = SettingsConfigDict(env_file=".env")
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,12 +1,16 @@
 from passlib.context import CryptContext
+import hashlib
 
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password):
-    return pwd_context.hash(password)
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+def normalize_password(password: str):
+    return hashlib.sha256(password.strip().encode()).hexdigest()
+
+
+def hash_password(password: str):
+    return pwd_context.hash(normalize_password(password))
+
+
+def verify_password(password: str, hashed_password: str):
+    return pwd_context.verify(normalize_password(password), hashed_password)
