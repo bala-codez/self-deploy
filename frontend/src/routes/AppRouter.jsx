91da98c304ab4afb33f891f7d1aba6b966e08e68
bala-codez <mohan144004@gmail.com>
2026-03-13 21:17:02 +0530

    feat(features/auth) : add pages to approuter in frontend

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -5,16 +5,35 @@ import RegisterPage from "../pages/RegisterPage.jsx";
 import VerifyOtpPage from "../pages/VerifyOtpPage.jsx";
 import OAuthCallbackPage from "../pages/OAuthCallbackPage.jsx";
 import ProtectedRoute from "./ProtectedRoute.jsx";
+import Layout from "../components/layouts/Layout.jsx";
+import DomainsPage from "../pages/DomainsPage.jsx";
+import DashboardPage from "../pages/DashboradPage.jsx";
+import AppsPage from "../pages/AppsPage.jsx";
+import LabsPage from "../pages/LabsPage.jsx";
+import MonitoringPage from "../pages/MonitoringPage.jsx";
+import SettingsPage from "../pages/SettingsPage.jsx";
+import ProfilePage from "../pages/ProfilePage.jsx";
+
+
 
 export default function AppRouter() {
     return (
         <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/otp" element={<VerifyOtpPage />} />
-            <Route path="/oauth/callback" element={<OAuthCallbackPage />} />
-            <Route element={<ProtectedRoute />}>
-                <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/register" element={<RegisterPage/>}/>
+            <Route path="/otp" element={<VerifyOtpPage/>}/>
+            <Route path="/oauth/callback" element={<OAuthCallbackPage/>}/>
+            <Route element={<ProtectedRoute/>}>
+                <Route path="/home" element={<HomePage/>}/>
+                <Route element={<Layout/>}>
+                    <Route path="/" element={<DashboardPage/>}/>
+                    <Route path="/apps" element={<AppsPage/>}/>
+                    <Route path="/labs" element={<LabsPage/>}/>
+                    <Route path="/domains" element={<DomainsPage/>}/>
+                    <Route path="/monitoring" element={<MonitoringPage/>}/>
+                    <Route path="/settings" element={<SettingsPage/>}/>
+                    <Route path="/profile" element={<ProfilePage/>}/>
+                </Route>
             </Route>
         </Routes>
     );
